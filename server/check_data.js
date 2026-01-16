457102660df14c6c7dbd226ca8f8b779731ea93a
Razii73 <rasikizhisseri42@gmail.com>
2026-01-16 12:21:35 +0530

    Initial commit (Clean)

--- server/check_data.js
+++
@@ -1,8 +0,0 @@
-const db = require('./database');
-
-db.serialize(() => {
-    db.get("SELECT Count(*) as count FROM student_years", (err, row) => console.log('Years:', row.count));
-    db.get("SELECT Count(*) as count FROM departments", (err, row) => console.log('Departments:', row.count));
-    db.get("SELECT Count(*) as count FROM transactions", (err, row) => console.log('Transactions:', row.count));
-    db.get("SELECT * FROM transactions LIMIT 5", (err, row) => console.log('Sample Tx:', row));
-});
